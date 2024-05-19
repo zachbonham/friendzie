7089626c18394fc07075db022286a13d6b290256
Zach Bonham <zbonham@virtuoso.com>
2024-05-19 11:16:41 -0500

    adding webapi refactoring in progress

---
+++ src/FriendZie.Api/GameEndpoints.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+
+namespace FriendZie.Api;
+
+
+public record NewGameRequest(string OwnerName) { }
+
+public record CreateNewGameComand(string OwnerName) : IRequest<CreateNewGameResult> { }
+public record CreateNewGameResult(string InvitationCode, string InviteUrl)  { }
+
+
+public record CreateNewGameResponse(string InvitationCode) { };
+
+
+
+public static class GameEndpoints
+{
+
+    public static void MapGameEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("games", CreateNewGame)
+
+            .WithName("CreateNewGame")             
+            .Produces<CreateNewGameResponse>()
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Creates a new friendzie game",
+                Description = "This is a description"
+            });
+
+
+    }
+
+    
+    public static async Task<IResult> CreateNewGame(NewGameRequest request, ISender sender)
+    {
+        var result = await sender.Send(new CreateNewGameComand(request.OwnerName));
+        var response = new CreateNewGameResponse(result.InvitationCode);
+        return Results.Ok(response);
+
+    }
+
+}
--- src/FriendZie.Api/Program.cs
+++ src/FriendZie.Api/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using FriendZie.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+
+app.MapGameEndpoints();
+
+app.Run();
